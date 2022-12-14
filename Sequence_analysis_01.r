e4cf122f8bbdff7678405d3f9962148d7d69b748
Rajarshi Ray <rayrajarshi9@gmail.com>
2022-12-14 23:51:55 +0530

    Sequence Counter DNA Bases -1

---
+++ Sequence_analysis_01.r
@@ -0,0 +1,39 @@
+# take user input of DNA sequence
+
+rangeIn <- as.integer(readline("Enter Limit of sequence : "))
+class(rangeIn)
+
+# Make empty vector and store nucleotides in it
+
+nset <- c()
+
+for (cn in 1:rangeIn) {
+  nBases <- readline("Enter Nucleotide Base :")
+  nset[cn] <- nBases
+}
+
+print(length(nset))
+
+# create nucleotide base counters
+
+countA = 0
+countT = 0
+countG = 0
+countC = 0
+
+for (i in 1:length(nset)) {
+  if(nset[i]=="A"){
+    countA = countA+1
+  }
+  else if(nset[i]=="T"){
+    countT=countT+1
+  }
+  else if(nset[i]=="G"){
+    countG = countG+1
+  }
+  else if(nset[i]=="C"){
+    countC = countC+1
+  }
+}
+
+
